93f0acbabdc103c04ef62a88a5ca40967a5e3c6e
toolateralus <joshnjoshgaming@gmail.com>
2024-04-18 11:46:38 -0400

    Refactor TimeSpanToStringConverter to floor the time span and format it as hh:mm:ss

--- Source/Converter.cs
+++ Source/Converter.cs
@@ -5,17 +5,21 @@ using Avalonia.Data.Converters;
 
 namespace LibreSplit.Convert;
 public class TimeSpanToStringConverter : IValueConverter {
-  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+  public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
     if (value is TimeSpan timeSpan) {
-      return timeSpan.ToString();
+      double totalSeconds = Math.Floor(timeSpan.TotalSeconds);
+      TimeSpan flooredTimeSpan = TimeSpan.FromSeconds(totalSeconds);
+      return flooredTimeSpan.ToString(@"hh\:mm\:ss");
     }
-    return AvaloniaProperty.UnsetValue;
+    return string.Empty;
   }
 
   public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
     if (value is string str) {
       if (TimeSpan.TryParse(str, out var timeSpan)) {
         return timeSpan;
+      } else {
+        Console.WriteLine("Unable to parse TimeSpan from given parameter : Likely the splits editor.");
       }
     }
     return AvaloniaProperty.UnsetValue;
