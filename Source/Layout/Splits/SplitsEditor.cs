af2068da3e334a53e530c4a57d206b7fb611b5a3
Cooper-Pilot <cooper.pilot@hotmail.com>
2025-09-24 19:23:39 -0400

    Splits layout comparison color editing

--- Source/Layout/Splits/SplitsEditor.cs
+++ Source/Layout/Splits/SplitsEditor.cs
@@ -14,5 +14,35 @@ public partial class SplitsEditor : UserControl {
       LayoutItem.ActiveBGColor = value.ToString();
     }  
   }
+  public Color InactiveBGColor {
+    get => Color.Parse(LayoutItem.InactiveBGColor);
+    set {
+      LayoutItem.InactiveBGColor = value.ToString();
+    }  
+  }
+  public Color AheadGainingTime {
+    get => Color.Parse(LayoutItem.AheadGainingTimeColor);
+    set {
+      LayoutItem.AheadGainingTimeColor = value.ToString();
+    }  
+  }
+  public Color AheadLosingTimeColor {
+    get => Color.Parse(LayoutItem.AheadLosingTimeColor);
+    set {
+      LayoutItem.AheadLosingTimeColor = value.ToString();
+    }  
+  }
+  public Color BehindGainingTimeColor {
+    get => Color.Parse(LayoutItem.BehindGainingTimeColor);
+    set {
+      LayoutItem.BehindGainingTimeColor = value.ToString();
+    }  
+  }
+  public Color BehindLosingTimeColor {
+    get => Color.Parse(LayoutItem.BehindLosingTimeColor);
+    set {
+      LayoutItem.BehindLosingTimeColor = value.ToString();
+    }  
+  }
   public SplitsLayout LayoutItem { get; internal set; }
 }
--- Source/Layout/Splits/SplitsLayout.cs
+++ Source/Layout/Splits/SplitsLayout.cs
@@ -4,20 +4,49 @@ namespace LibreSplit;
 
 public class SplitsLayout : LayoutItem {
   private string activeBGColor = "Purple";
-  private string inactiveBGColor = "Transparent";
-
   public string ActiveBGColor {
     get => activeBGColor; set {
       activeBGColor = value;
       OnPropertyChanged();
     }
   }
+
+  private string inactiveBGColor = "Transparent";
   public string InactiveBGColor {
     get => inactiveBGColor; set {
       inactiveBGColor = value;
       OnPropertyChanged();
     }
   }
+  private string aheadGainingTimeColor = "Green";
+  public string AheadGainingTimeColor {
+    get => aheadGainingTimeColor; set {
+      aheadGainingTimeColor = value;
+      OnPropertyChanged();
+    }
+  }
+  private string aheadLosingTimeColor = "LightGreen";
+  public string AheadLosingTimeColor {
+    get => aheadLosingTimeColor; set {
+      aheadLosingTimeColor = value;
+      OnPropertyChanged();
+    }
+  }
+  private string behindGainingTimeColor = "IndianRed";
+  public string BehindGainingTimeColor {
+    get => behindGainingTimeColor; set {
+      behindGainingTimeColor = value;
+      OnPropertyChanged();
+    }
+  }
+  private string behindLosingTimeColor = "Red";
+  public string BehindLosingTimeColor {
+    get => behindLosingTimeColor; set {
+      behindLosingTimeColor = value;
+      OnPropertyChanged();
+    }
+  }
+
   public override Control? Control => new SplitsControl(this);
   public override Control? Editor => new SplitsEditor(this);
   public override string LayoutItemName => "Splits";
